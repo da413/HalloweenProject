37f989c39888d3e42376fe46fade8824025b3e78
Daniel Wolf <danielwolfcol@gmail.com>
2024-10-23 15:57:49 -0400

    initial commit

---
+++ HalloweenProject/src/HalloweenRunner.java
@@ -0,0 +1,118 @@
+//Daniel Wolf , 9/28/2024 , COP3330, Assignment 3: Method Overloading,Comparable, and File I/O
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+import java.util.Collections;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HalloweenRunner {
+
+	public static void main(String[] args) throws IOException {
+	
+	
+	Scanner askfile = new Scanner(System.in);
+	String inputfilename = askfile.next();
+	File costumefile = new File(inputfilename);
+	String outputfilename;
+
+	HalloweenCostumes costume = new HalloweenCostumes();
+	
+	ArrayList<HalloweenCostumes> costumeslist = new ArrayList<HalloweenCostumes>();
+	String costumeName;
+	double costumeCost;
+	int WantScore;
+	try {
+	askfile = new Scanner(costumefile); 
+	int numtimes = askfile.nextInt();
+	for(int i = 0; i < numtimes; i++) {
+		
+		costumeName = askfile.next(); costumeCost = askfile.nextDouble(); WantScore = askfile.nextInt();
+		//System.out.println(costumeName + costumeCost + WantScore);
+		
+		 if(costumeName.length() <= 2 && costumeCost == -1 && WantScore == -1) {
+			costume = new HalloweenCostumes();
+		}
+		
+		 else if(WantScore == -1 && costumeCost == -1) {
+			costume = new HalloweenCostumes(costumeName);
+				
+		}
+		 
+		 else if(costumeName.length() <= 2 && WantScore == -1) {
+			 costume = new HalloweenCostumes(costumeCost);
+		 }
+		 
+		 else if(costumeName.length()<=2 && costumeCost == -1) {
+			 costume = new HalloweenCostumes(WantScore);
+		 }
+		
+		else if(WantScore == -1) {
+			costume = new HalloweenCostumes(costumeName,costumeCost);
+			
+		}
+		
+		else if(costumeCost == -1) {
+			costume = new HalloweenCostumes(costumeName, WantScore);
+			
+		}
+		
+		
+		
+		else if (costumeName.length() <=2) {
+			costume = new HalloweenCostumes(costumeCost, WantScore);
+			
+		}
+		
+		else {
+		
+		costume = new HalloweenCostumes(costumeName, costumeCost, WantScore);
+		
+		}
+			
+		
+		costumeslist.add(costume);
+		}
+	
+
+
+	for(HalloweenCostumes e: costumeslist) {
+		
+		e.CalcCoolness();
+	
+		
+	}	Collections.sort(costumeslist);
+	
+	askfile = new Scanner(System.in);
+	outputfilename = askfile.next();
+	FileWriter filewriter = new FileWriter(outputfilename);
+	
+	for(HalloweenCostumes i: costumeslist) {
+		System.out.println(i.getName() +  " " + i.getCoolness() + " " + i.getCost() + " " + i.getWant());
+		filewriter.write(i.getName() +  " " + i.getCoolness() + " " + i.getCost() + " " + i.getWant() + "\n");
+	}  //this part to write into new output file
+	
+	filewriter.close();
+	
+	
+	}	catch(FileNotFoundException e) {
+			System.out.println("An error occured");
+			e.printStackTrace();
+	}
+		catch(NoSuchElementException u) {
+			
+			u.printStackTrace();
+		}
+
+
+	
+	
+	
+		     
+	}	
+}		   
+	
+
+
---
+++ src/HalloweenCostumes.java
@@ -0,0 +1,98 @@
+
+public class HalloweenCostumes implements Comparable<HalloweenCostumes>{
+	
+		private String CostumeName;
+		private double CostumeCost;
+		private int WantFactor;
+		private int CoolnessScore = 0;
+		
+		public HalloweenCostumes() {
+			this.CostumeName = "Super_Awesome_Costume";
+			this.CostumeCost = 39.99;
+			this.WantFactor = 3;
+		}
+		
+		public HalloweenCostumes(String name) {
+			this.CostumeName = name;
+			this.CostumeCost = 39.99;
+			this.WantFactor = 3;
+		}
+		
+		public HalloweenCostumes(String name, double cost) {
+			this.CostumeName = name;
+			this.CostumeCost = cost;
+			this.WantFactor = 3;
+		}
+		
+		public HalloweenCostumes(String name, int want) {
+			this.CostumeName = name;
+			this.WantFactor = want;
+			this.CostumeCost = 39.99;
+		}
+		
+		public HalloweenCostumes(String name, double cost, int want) {
+			this.CostumeName = name;
+			this.CostumeCost = cost;
+			this.WantFactor = want;
+		}
+		
+		public HalloweenCostumes(double cost) {
+			this.CostumeCost = cost;
+			this.CostumeName = "Super_Awesome_Costume";
+			this.WantFactor = 3;
+		}
+		
+		public HalloweenCostumes(double cost, int want) {
+			this.CostumeCost = cost;
+			this.WantFactor = want;
+			this.CostumeName = "Super_Awesome_Costume";
+			
+		}
+		
+		public HalloweenCostumes(int want) {
+			this.WantFactor = want;
+			this.CostumeCost = 39.99;
+			this.CostumeName = "Super_Awesome_Costume";
+		}
+		
+		public void CalcCoolness() { 
+			this.CoolnessScore = Math.round((int)(this.CostumeName.length() * this.WantFactor / this.CostumeCost));
+			
+		}
+		
+		public String getName() {
+			return this.CostumeName;
+		}
+		
+		public double getCost() {
+			return this.CostumeCost;
+		}
+		
+		public int getWant() {
+			return this.WantFactor;
+		}
+		
+		public int getCoolness() {
+			return this.CoolnessScore;
+		}
+		
+	
+
+	@Override
+	public int compareTo(HalloweenCostumes o) {
+	  if(this.CoolnessScore - o.CoolnessScore > 0) {
+		  return -1;
+	  }
+	  
+	  if(this.CoolnessScore - o.CoolnessScore < 0) {
+		  return 1;
+	  }
+	  
+	  if(this.CoolnessScore - o.CoolnessScore == 0) {
+		  return this.CostumeName.compareTo(o.CostumeName);
+	  }
+	  return 0;
+		
+	}
+
+}
